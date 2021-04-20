b384a860b4619b25ee40a5dc622e15794a36c1d9
leandrors <leandrors@ciandt.com>
2021-04-20 10:30:34 -0300

    feat: add scm versioning

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup, find_packages
-from kubespin import VERSION
 
 
 setup(
     name="kubespin",
-    version=VERSION,
+    use_scm_version=True,
     url="https://github.com/lersouza/kubespin",
     author="Leandro Rodrigues de Souza",
     license="MIT",
     scripts=["bin/kubespin"],
     include_package_data=True,
     description="A simple to to manage Spinnaker pipelines declaratively.",
-    install_requires=[
-        "jsonnet"
-    ],
-    packages=find_packages()
+    install_requires=["jsonnet"],
+    setup_requires=["setuptools_scm"],
+    packages=find_packages(),
 )
